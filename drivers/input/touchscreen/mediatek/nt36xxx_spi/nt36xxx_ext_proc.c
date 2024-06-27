5a7165841a5bd76e1a915c5b6a98a2bd0c169cd0
SonzaiX シ <abhiedthea@gmail.com>
2024-06-27 09:47:45 +0000

    x

--- drivers/input/touchscreen/mediatek/nt36xxx_spi/nt36xxx_ext_proc.c
+++ drivers/input/touchscreen/mediatek/nt36xxx_spi/nt36xxx_ext_proc.c
@@ -1714,7 +1714,7 @@ static ssize_t nvt_edge_reject_switch_proc_write(struct file *filp,const char __
 
 	ret = count;
 out:
-	char *tmp_buf = NULL;
+	//char *tmp_buf = NULL;
 	NVT_LOG("--\n");
 	return ret;
 }
